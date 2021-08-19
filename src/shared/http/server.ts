0853693aaa65b634b284f67ee77f3d4837aa053d
DaniSan0508-project <danielsantoscarneiro0508@gmail.com>
2021-08-19 08:59:43 -0300

    add typeorm cli and setting typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
+import 'reflect-metadata';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
-import 'reflect-metadata';
+import '@shared/typeorm';
 
 const app = express();
 
