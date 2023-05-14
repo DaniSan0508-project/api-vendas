d71566155deaae63e13bd7989f8a0b1261c5280d
Daniel Santos <danielsantos@sysfar.com.br>
2023-05-14 00:30:09 -0300

    feat:implementado controllers de usuarios

---
+++ src/modules/users/controllers/UsersControllers.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import ListUserService from '../service/ListUserService';
+import CreateUserService from '../service/CreateUserService';
+
+class UsersControllers {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUserService = new ListUserService();
+    const users = await listUserService.execute();
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+    const createUserService = new CreateUserService();
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+    });
+    return response.json(user);
+  }
+}
+
+export default UsersControllers;
