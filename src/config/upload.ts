2a101d32c037caab56d8fab2f180d934f2e7ffe6
Daniel Santos <danielsantos@sysfar.com.br>
2023-05-19 22:22:25 -0300

    feat:implementado multer

---
+++ src/config/upload.ts
@@ -0,0 +1,17 @@
+import multer from 'multer';
+import path from 'path';
+import crypto from 'crypto';
+const uploadeFolder = path.resolve(__dirname, '..', '..', 'uploads');
+
+export default {
+  directory: uploadeFolder,
+  storage: multer.diskStorage({
+    destination: uploadeFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const filename = `${fileHash}-${file.originalname}`;
+
+      callback(null, filename);
+    },
+  }),
+};
