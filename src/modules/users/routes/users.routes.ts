0e2faa2d3bc138e0cefa0169862af0b1de9fe5c8
Daniel Santos <danielsantos@sysfar.com.br>
2023-05-20 15:50:29 -0300

    feat:implementado multer para imagens avatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -2,9 +2,15 @@ import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import UsersControllers from '../controllers/UsersControllers';
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import multer from 'multer';
+import upload from '@config/upload';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersControllers();
+const usersAvatarController = new UserAvatarController();
+
+const uploadMulter = multer(upload);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 usersRouter.post(
@@ -17,6 +23,12 @@ usersRouter.post(
     },
   }),
   usersController.create,
+);
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  uploadMulter.single('avatar'),
+  usersAvatarController.create,
 );
 
 export default usersRouter;
